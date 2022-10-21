4d714ee6812d8a3538f960350d282a21e0610129
Boris Fazio <boris.fazio@outlook.com>
2022-10-21 04:01:35 +0200

    added PPSS name to district list

--- R/1_setup.R
+++ R/1_setup.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(tidyr)
 library(readxl)
+library(stringi)
 library(writexl)
 library(fuzzyjoin)
 library(lubridate)
--- main.R
+++ main.R
@@ -1,7 +1,8 @@
+daterange_vector <- c("092022", "102022") # for regional AT
+# PENDING - make the range extensive to district AT
+
 source("R/1_setup.R", encoding = "utf-8")
 source("R/2_load.R", encoding = "utf-8")
-
-daterange_vector <- c("092022", "102022") # for regional AT
 
 forms.at %>%
   transmute(
@@ -10,6 +11,7 @@ forms.at %>%
     PROVINCIA = simpletext(Provincia),
     DISTRITO = simpletext(Distrito),
     FECHA = as_date(`Fecha de la Asistencia Técnica`),
+    RESPONSABLE = simpletext(`Unidad Orgánica o Programa Social del responsable que brinda la asistencia técnica`),
     CARGO = simpletext(`Cargo del responsable`),
     INTERVENCION = `Intervención en la que se brindó asistencia técnica`,
     RECIBE = `Institución que recibió la asistencia técnica`
@@ -22,23 +24,29 @@ forms.at %>%
     DEPARTAMENTO =
       case_when(DEPARTAMENTO == "LIMA METROPOLITANA" ~ "LIMA M",
                 TRUE ~ DEPARTAMENTO),
+    RESPONSABLE =
+      case_when(
+        CARGO %in% c(
+          "COORDINADOR DE ENLACE", "CORDINADOR ENLACE MIDIS"
+          ) ~ "DIRECCION DE ARTICULACION TERRITORIAL",
+        TRUE ~ RESPONSABLE
+      ),
     CATEGORIA =
       case_when(
           RECIBE == "Equipo Territorial MIDIS (Regional)" &
-          (grepl("CE", CARGO)|
-          CARGO == "COORDINADOR TERRITORIAL") ~ "AT_ETR",
+            RESPONSABLE == "DIRECCION DE ARTICULACION TERRITORIAL" ~ "AT_ETR",
           RECIBE == "Equipo Territorial MIDIS (Local)" &
-          (grepl("CE", CARGO)|
-          CARGO == "COORDINADOR TERRITORIAL") ~ "AT_ETL",
+            RESPONSABLE == "DIRECCION DE ARTICULACION TERRITORIAL" ~ "AT_ETL",
           RECIBE %in% c("Gobierno Regional", "IAR") &
-          (grepl("CE", CARGO)|
-          CARGO == "COORDINADOR TERRITORIAL") ~ "AT_GORE",
+            RESPONSABLE == "DIRECCION DE ARTICULACION TERRITORIAL" ~ "AT_GORE",
           RECIBE %in% c("Gobierno Local", "IAL") &
-          (grepl("CE", CARGO)|
-          CARGO == "COORDINADOR TERRITORIAL") ~ "AT_GL",
+            RESPONSABLE == "DIRECCION DE ARTICULACION TERRITORIAL" ~ "AT_GL",
           RECIBE %in% c("Gobierno Local", "IAL") &
-            grepl("GESTO|GRSTO|ACOMP|ACONP|MONIT|ASIST|CTZ|ZONAL|^AT$", toupper(CARGO)) ~ "AT_PPSS")
-  ) -> at.pretidy
+            RESPONSABLE %in%
+            c("CUNA MAS", "JUNTOS", "PAIS", "PENSION 65", "QALI WARMA") ~ "AT_PPSS"),
+    FECHA = case_when(N_ROW==156 ~ dmy(23082022),
+                      TRUE ~ FECHA)
+  ) %>% select(-CARGO) -> at.pretidy
 
 ###
 at.pretidy %>% filter(is.na(CATEGORIA)) %>% write_xlsx("mid/exclusiones-at.xlsx")
@@ -100,22 +108,38 @@ fuzz_listo %>%
     at.tidy %>%
       filter(FECHA >= "2022-08-01") %>%
       transmute(N_ROW,
-                name = paste0(CATEGORIA, "_", month(FECHA)),
-                value = 1),
+                name = CATEGORIA,
+                fecha = paste0(sprintf("%02d", month(FECHA)), year(FECHA)),
+                type = ifelse(RESPONSABLE == "DIRECCION DE ARTICULACION TERRITORIAL",
+                              "CE", "PPSS"),
+                value = RESPONSABLE),
     by = "N_ROW"
   ) %>% ### REVISAR Q ONDA CON N_ROW == 156, POR AHORA LO IGNORAMOS
   filter(N_ROW != 156) %>%
-  select(-N_ROW) %>%
-  unique %>%
-  pivot_wider(values_fill = 0) %>%
-  left_join(egtpi %>% select(UBIGEO, DEPARTAMENTO, PROVINCIA, DISTRITO),
-            .,
-            by = "UBIGEO") %>%
-  replace_na(list(AT_GL_8 = 0,
-                  AT_PPSS_8 = 0,
-                  AT_GL_9 = 0,
-                  AT_PPSS_9 = 0,
-                  AT_GL_10 = 0,
-                  AT_PPSS_10 = 0)) %>%
+  group_by(UBIGEO, name, type) %>%
+  filter(N_ROW == min(N_ROW)) %>%
+  ungroup %>%
+  select(-N_ROW, -type) -> at_local
+
+#
+egtpi %>%
+  select(UBIGEO, DEPARTAMENTO, PROVINCIA, DISTRITO) %>%
+  crossing(tibble(fecha = c("082022", daterange_vector))) %>%
+  crossing(tibble(name = c("AT_GL", "AT_PPSS", "CARGO_PPSS"))) %>%
+  left_join(
+    bind_rows(
+      at_local %>% mutate(value = "1"),
+    at_local %>%
+      filter(value != "DIRECCION DE ARTICULACION TERRITORIAL") %>%
+      mutate(name = "CARGO_PPSS")),
+    by = c("UBIGEO", "fecha", "name")) %>%
+  mutate(
+    value = case_when(
+      name != "CARGO_PPSS" & is.na(value) ~ "0",
+      TRUE ~ value
+    ),
+    name = paste0(name, "_", fecha)) %>%
+  select(-fecha) %>%
+  pivot_wider() %>%
   arrange(UBIGEO) %>%
   write_xlsx("at-local.xlsx")
